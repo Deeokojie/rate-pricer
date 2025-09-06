f85cc32fa7eb487e44fecb9dccb534eb52fe4c75
Deeokojie <117442106+Deeokojie@users.noreply.github.com>
2025-09-06 21:27:56 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,14 +4,11 @@ from app.models.user import User
 from app.routes import users
 from app.routes import pricing
 
-# Create the FastAPI app instance first
 app = FastAPI()
 
-# Then include your routes
 app.include_router(users.router)
 app.include_router(pricing.router)
 
-# Then create tables
 Base.metadata.create_all(bind=engine)
 
 
